988e287ba99dba3e1a3e4f0bd3ae81821f34cba0
Luis <rluis747@gmail.com>
2022-03-10 17:22:57 -0500

    Soy luis

--- clientReact/src/container/Footer/Footer.jsx
+++ clientReact/src/container/Footer/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">
+          <a href="mailto:rluis747@gmail.com" className="p-text">
             rluis747@gmail.com
           </a>
         </div>
